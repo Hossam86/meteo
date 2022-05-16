3da197570da282cf22a7ee20b1c59e889f7fe164
Hossam Samir <hossamsamir@outlook.com>
2022-05-16 17:44:01 +0200

    main app file

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+# For security reasons, many modern web frameworks actually limit incoming connections to only come from localhost.
+# This means that our Flask app is running, but only accessible from within the Docker container, not from our Mac host.
+# The easiest way to fix this is to add `host=’0.0.0.0’` as a keyword argument to `app.run()` in main.py:
 
+# This change tells Flask to listen not only to requests from localhost (in this case the docker container),
+# but on all network connections. In the case of our container this means we can access it from our host,
+# and depending on configuration from other computers in our network.
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
